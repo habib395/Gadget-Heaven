04509131930713acb0a0d72869fdbfa014111cbd
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-11-12 21:11:11 +0600

    navbar section design done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HiOutlineShoppingCart } from "react-icons/hi";
 import { FaHeart } from "react-icons/fa";
-import { NavLink } from 'react-router-dom';
-
+import { NavLink, useLocation } from 'react-router-dom';
+import '../App.css'
+ 
 const Navbar = () => {
+  const location = useLocation()
+  useEffect(() =>{
+    console.log(location.pathname)
+  },[location])
     return (
-        <div className="navbar bg-base-100 container mx-auto">
+      <div>
+        <div className={`${location.pathname === '/'? 'navbar w-11/12 mx-auto active px-10': 'navbar bg-base-100 w-11/12 mx-auto'}`}>
         <div className="navbar-start">
-          <NavLink to='/'>Gadget Heaven</NavLink>
+          <NavLink className={`${location.pathname === '/'? 'active': ''}`} to='/'>Gadget Heaven</NavLink>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li><NavLink to='/'>Home</NavLink></li>
+            <li><NavLink className={`${location.pathname === '/'? 'active': ''}`} to='/'>Home</NavLink></li>
             <li><NavLink to='/statistic'>Statistic</NavLink></li>
             <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
             <li><NavLink to='/feedback'>Feedback</NavLink></li>
           </ul>
         </div>
         <div className="navbar-end flex gap-4">
-          <div className='border rounded-full p-3'> 
+          <div className='border rounded-full p-3 bg-white text-black'> 
         <HiOutlineShoppingCart />
           </div>
-          <div className='border rounded-full p-3'> 
+          <div className='border rounded-full p-3 bg-white text-black'> 
         <FaHeart />
           </div>
         </div>
       </div>
+
+
+
+
+
+      {/* <div className="p-2 mx-auto pt-4 w-2/3 relative py-60">
+      <div className='hero mx-auto rounded-lg absolute'>
+           <div className='bg-transparent rounded-lg'>
+           <img className="w-full border-2 rounded-lg p-2 h-full object-cover" src="./assets/banner.jpg" alt="" />
+           </div>
+            </div>
+      </div> */}
+      </div>
+      
     );
 };
 
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
     return (
         
        <div>
-         <div className='bg-[#9538E2] py-32 mb-52 rounded-lg w-11/12 mx-auto text-center'>
+         <div className='bg-[#9538E2] py-32 mb-52  w-11/12 mx-auto text-center'>
             {/* heading */}
             <Heading title='Upgrade Your Tech Accessorize with Gadget Heaven Accessories' subtitle='Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!'></Heading>
             {/* banner */}
